fb15d8ea4de009efb73bdaaf05bdb1484b08619e
Crystal Schooling <bytesizemagic@gmail.com>
2020-02-15 21:33:10 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,11 +3,8 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == ""
     return false
-  elsif board[index] == "X"
+  elsif board[index] == "X" || board[index] == "O"
     return true
-  elsif board[index] == "O"
-    return true
-
   end
 
 
